7aec07143fed05ffc1f2bbd343458c467c359571
Stan <stanrutten242@hotmail.com>
2017-05-16 11:24:45 +0200

    This is the correct one
    added small body

--- index.php
+++ index.php
@@ -38,46 +38,58 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<header>
+	<nav class="navbar navbar-inverse" style="border-radius: 0px; background-color: darkorange;">
+	  <div class="container-fluid">
+	    <div class="navbar-header"> 
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>                        
+	      </button>
+	    </div>
+	    <div class="collapse navbar-collapse" id="myNavbar">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a style="background-color: darkorange;" href="#">Home</a></li>
+	        <li class="dropdown">
+	          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Locaties<span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Drenthe</a></li>
+	            <li><a href="#">Flevoland</a></li>
+	            <li><a href="#">Friesland</a></li>
+	            <li><a href="#">Gelderland</a></li>
+	            <li><a href="#">Groningen</a></li>
+	            <li><a href="#">Limburg</a></li>
+	            <li><a href="#">Noord-Brabant</a></li>
+	            <li><a href="#">Noord-Holland</a></li>
+	            <li><a href="#">Overijssel</a></li>
+	            <li><a href="#">Utrecht</a></li>
+	            <li><a href="#">Zeeland</a></li>
+	            <li><a href="#">Zuid-Holland</a></li>
+	          </ul>
+	        </li>
+	        <li><a href="#">About</a></li>
+	        <li><a href="#">FAQ</a></li>
+	      </ul>
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+	        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+</header>
 
-<nav class="navbar navbar-inverse" style="border-radius: 0px; background-color: darkorange;">
-  <div class="container-fluid">
-    <div class="navbar-header"> 
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a style="background-color: darkorange;" href="#">Home</a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Locaties<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Drenthe</a></li>
-            <li><a href="#">Flevoland</a></li>
-            <li><a href="#">Friesland</a></li>
-            <li><a href="#">Gelderland</a></li>
-            <li><a href="#">Groningen</a></li>
-            <li><a href="#">Limburg</a></li>
-            <li><a href="#">Noord-Brabant</a></li>
-            <li><a href="#">Noord-Holland</a></li>
-            <li><a href="#">Overijssel</a></li>
-            <li><a href="#">Utrecht</a></li>
-            <li><a href="#">Zeeland</a></li>
-            <li><a href="#">Zuid-Holland</a></li>
-          </ul>
-        </li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">FAQ</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+         <div id="park-finder">
+			<div id="slogan"><li>Vindt de leukste en de dichtsbijzijnste pretparken!</li></div>	
+			<div id="address-search">
+				<input type="text" name="address" class="address" placeholder="bv. Janssenweg 73 / 5961 RM">
+			</div>
+			<div id="submit-button">
+				<input type="submit" name="submit-button" value="Zoek!">
+			</div>
+		</div>
+
 <!--<div class="banner">
   <img src="img/banner3.jpeg" width="100%" height="250px;">
 </div>-->
