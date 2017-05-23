3ea36898ce2830e0b734e770647ddb5c0e9f5004
Jim Moorrees <jimapestaartje@gmail.com>
2017-05-23 10:05:45 +0200

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -1,30 +1,3 @@
-<?php 
-	include("Conn.php");
-	//include("Function.php");
-
-	if(isset($_POST['ParkNaam']) && isset($_POST['ParkLocatie']) && isset($_POST['ParkOpeningsDagen']) && isset($_POST['ParkOpeningsTijden']) && isset($_POST['ParkPrijzen']) && isset($_POST['ParkLeeftijden']) && isset($_POST['ParkTags']) && isset($_POST['ParkBeschrijving'])) {
-		$req=$bdd->prepare('INSERT INTO park(ParkNaam,ParkLocatie,ParkOpeningsDagen,ParkOpeningsTijden,ParkPrijzen,ParkLeeftijden,ParkTags,ParkBeschrijving) 
-		VALUES(:ParkNaam, :ParkLocatie, :ParkOpeningsDagen, :ParkOpeningsTijden, :ParkPrijzen, :ParkLeeftijden, :ParkTags, :ParkBeschrijving)');	
-		$req->execute(array(
-			'ParkNaam'=>$_POST['ParkNaam'],
-			'ParkLocatie'=>$_POST['ParkLocatie'],
-			'ParkOpeningsDagen'=>$_POST['ParkOpeningsDagen'],
-			'ParkOpeningsTijden'=>$_POST['ParkOpeningsTijden'],
-			'ParkPrijzen'=>$_POST['ParkPrijzen'],
-			'ParkLeeftijden'=>$_POST['ParkLeeftijden'],
-        	'ParkTags'=>$_POST['ParkTags'],
-        	'ParkBeschrijving'=>$_POST['ParkBeschrijving']
-			));
-		}
-
-		$query = "SELECT * FROM `park'";
-		$result = $connect->query($query);
-
-		
-?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
