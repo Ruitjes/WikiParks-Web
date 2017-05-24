f147f4bd43f38d03ef247df01c438e3821440328
Jim Moorrees <jimapestaartje@gmail.com>
2017-05-24 14:06:08 +0200

    this stuff should work
    deleted and uploaded everything because the other stuff didnt seem to work anymore

---
+++ index2.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>WikiParks</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="css/stylesheet.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse" style="border-radius: 0px; background-color: darkorange;">
+  <div class="container-fluid">
+    <div class="navbar-header"> 
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a style="background-color: darkorange;" href="#">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Locaties<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Drenthe</a></li>
+            <li><a href="#">Flevoland</a></li>
+            <li><a href="#">Friesland</a></li>
+            <li><a href="#">Gelderland</a></li>
+            <li><a href="#">Groningen</a></li>
+            <li><a href="#">Limburg</a></li>
+            <li><a href="#">Noord-Brabant</a></li>
+            <li><a href="#">Noord-Holland</a></li>
+            <li><a href="#">Overijssel</a></li>
+            <li><a href="#">Utrecht</a></li>
+            <li><a href="#">Zeeland</a></li>
+            <li><a href="#">Zuid-Holland</a></li>
+          </ul>
+        </li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!--<div class="banner">
+  <img src="img/banner3.jpeg" width="100%" height="250px;">
+</div>-->
+<div class="container-fluid">
+      <nav class="fixed-left">
+        <h4>Zoeken op leeftijd</h4>
+        <h5><a href="#">0-5 jaar</a></h5><br/>
+        <h5><a href="#">5-10 jaar</a></h5><br/>
+        <h5><a href="#">10-13 jaar</a></h5><br/>
+        <h5><a href="#">13+ jaar</a></h5><br/>
+        <h4>Zoeken op Entree Prijs</h4>
+        <h5><a href="#">5-10 Euro</a></h5><br/>
+        <h5><a href="#">10-15 Euro</a></h5><br/>
+        <h5><a href="#">15-20 Euro</a></h5><br/>
+        <h5><a href="#">25+ Euro</a></h5><br/>
+      </nav>
+</div>
+<div class="headingtext">
+<img width="300px;" height="75px;" src="img/logowikiparks.png">
+</div>
+
+<div class="pretpark-container">
+  <div class="pretpark-image"><img src="" width="150px" height="100px"></div>
+  <div class="pretpark-informatie">
+  <div class="pretpark-naam"><h3>naam1</h3></div>
+  <div class="pretpark-locatie">rotterdam</div>
+  <div class="pretpark-openingstijden">00:00 - 00:00</div>
+  <div class="pretpark-prijs">€99,99</div>
+  
+
+  </div>
+
+</div>
+
+</body>
+</html>
